95b5f9ac3014343e9008c65d7918ed890cd3c04b
davexz17 <davidunabia11@gmail.com>
2025-08-08 00:26:22 -0700

    I added banking activity

--- src/davidproj/Davidproj.java
+++ src/davidproj/Davidproj.java
@@ -1,21 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package davidproj;
 
-/**
- *
- * @author PC 10
- */
+import java.util.Scanner;
 public class Davidproj {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+    Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Enter name: ");
+        String name = sc.nextLine();
+        
+        System.out.println("Enter birth year: ");
+        int by =sc.nextInt();
+        
+        int age = 2025 - by;
+        
+        System.out.println("\nhello "+name+"your age is "+age);
     }
+    
     
 }
--- src/davidproj/davidpjoc2.java
+++ src/davidproj/davidpjoc2.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package davidproj;
 
-/**
- *
- * @author PC 10
- */
 public class davidpjoc2 {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello kalibutan");
+    }
 }
